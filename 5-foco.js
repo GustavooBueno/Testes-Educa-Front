2811da7eb458acb0edf242b9e2cb15215483fa90
Gustavo Bueno <89856818+GustavooBueno@users.noreply.github.com>
2023-07-21 09:43:02 -0300

    Add files via upload

---
+++ 5-foco.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+
+  try {
+    await page.goto('https://educa-front.vercel.app/');
+    await page.waitForTimeout(1000);
+
+    await page.type('input[type="email"]', 'teste@gmail.com');
+    await page.waitForTimeout(1000);
+
+    await page.type('input[type="password"]', 'teste8231@@');
+    await page.waitForTimeout(3000);
+
+    await page.click('button[type="submit"]');
+
+    await page.waitForTimeout(3000);
+
+    await page.click('img[alt="Texto"]');
+    await page.waitForTimeout(1000);
+
+    await page.click('img[alt="Foco"]');
+    await page.keyboard.press('PageDown');
+
+  } catch (error) {
+
+  } finally {
+    //await browser.close();
+  }
+})();
